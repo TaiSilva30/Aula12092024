794ecad4a9d660aa6b8bc9f74a140e5f6683fd31
Tai Silva30 <tatihsilva16@gmail.com>
2024-09-12 19:44:36 -0300

    atualização desafios

---
+++ Desafio6.py
@@ -0,0 +1,9 @@
+infolivros = {"titulo": "uma familia feliz", "autor": "rafael montes", "ano": "2024"}
+print(infolivros)
+
+
+addano = input("altere o ano de lançamento do livro: ")
+
+infolivros["ano"] = addano
+
+print("segue dados atualizados: ", infolivros)
---
+++ Desafio7.py
@@ -0,0 +1,9 @@
+aluno_notas = {"tailaine": "10", "felipe": "10", "amanda": "8", "ana": "5"}
+print(aluno_notas)
+
+aluno = input("digite o nome do aluno: ")
+
+if aluno in aluno_notas:
+    print(f"o aluno {aluno} esta com a nota: {aluno_notas[aluno]} ")
+else: 
+    print("aluno não existe na lista, tente novamente")
